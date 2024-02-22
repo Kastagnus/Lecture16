7ac3f5e63dd5daa7a476c6189bf740cd38a8aa21
Kastagnus <ucha95@gmail.com>
2024-02-23 00:24:25 +0400

    finished task22

--- task22.py
+++ task22.py
@@ -1,5 +1,6 @@
 import json
 import pickle
+import dill
 
 
 class Animal():
@@ -14,48 +15,84 @@ class Animal():
 
 
 myanimal = Animal("rex", "2", "Doggo")
-object = {"name": "will", "age": "28", "university": "Cambridge"}
+student = {"name": "will", "age": "28", "university": "Cambridge"}
+lambda_func = lambda x: x ** 3
 
 
 def serialization(object, filename="object.csv"):
     pickle_ser = False
+    dill_ser = False
     try:
         with open(filename, "w") as file:
             json.dump(object, file, indent=4)
-        print("JSON was successful with this object")
+        print("JSON was successful with serialization this object")
     except:
         print("Can not be serialized by JSON")
         pickle_ser = True
 
     while pickle_ser:
-        with open(filename, 'wb') as file:
-            pickle.dump(object, file)
-            print("Pickle was successful with this object")
+        try:
+            with open(filename, 'wb') as file:
+                pickle.dump(object, file)
+                print("Pickle was successful with serialization this object")
+                break
+        except:
+            dill_ser = True
+            print("Can not be serialized by pickle")
             break
+    while dill_ser:
+
+        with open(filename, 'wb') as file:
+            dill.dump(object, file)
+            print("Dill was successful with serialization this object")
+            break
+
+
 
 
 def deserialization(filename="object.csv"):
     pickle_deser = False
+    dill_deser = False
     try:
         with open(filename, 'r') as file:
-            object = json.load(file)
-            print(object)
-            print("JSON was successful with this object")
+            obj = json.load(file)
+            print("JSON was successful with deserialization this object")
     except:
-        print("Can not deserialized by JSON")
+        print("Can not deserialized by JSON ")
         pickle_deser = True
 
     while pickle_deser:
+        try:
+            with open(filename, mode='rb') as file:
+                obj = pickle.load(file)
+                print("Pickle was successful with deserialization this object")
+                break
+        except:
+            dill_deser = True
+            print("Can not be deserialized by pickle ")
+            break
+    while dill_deser:
         with open(filename, mode='rb') as file:
-            object = pickle.load(file)
-            object.describe_pet()
-            print("Pickle was successful with this object")
+            obj = dill.load(file)
+            print("Dill was successful with deserialization this object")
             break
 
+    return obj
+
+
 # # ჩაწერს და წაიკითხავს JSON-ი
-# serialization(object)
-# deserialization("object.csv")
+# serialization(student)
+# obj = deserialization("object.csv")
+# print(obj)
 
 # # ცდის ჩაწერას და წაკითხვას ჯეისნოთ, თუ არ გამოვიდა მიმართავს pickle-ს
 # serialization(myanimal)
-# deserialization("object.csv")
+# obj = deserialization("object.csv")
+# obj.describe_pet()
+
+# #ცდის ჩაწერას ჯეისონით, მერე pickle-ით, ბოლოს თუ ესენი წარუმატებელია მიმართვს Dill-ს
+# serialization(lambda_func)
+# obj = deserialization("object.csv")
+# print(obj(2))
+
+
